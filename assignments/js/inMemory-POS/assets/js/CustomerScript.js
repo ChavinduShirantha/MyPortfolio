247bb36c185e52a16d52818e1c76924aa08c2590
ChavinduShirantha <chavindushirantha@gmail.com>
2023-06-07 18:31:54 +0530

    Updated inMemory-POS

--- assignments/js/inMemory-POS/assets/js/CustomerScript.js
+++ assignments/js/inMemory-POS/assets/js/CustomerScript.js
@@ -1,13 +1,53 @@
+
+var customerDB=[];
 $("#saveCustomer").click(function () {
     let id = $("#customer-id").val();
     let name = $("#customer-name").val();
     let address = $("#customer-address").val();
     let salary = $("#customer-salary").val();
 
-    let tbody = "<tr>" + "<td>" + id + "</td>" + "<td>" + name + "</td>" + "<td>" + address + "</td>" + "<td>" + salary + "</td>" + "</tr>";
+    // let customer=[id,name,address,salary];
 
-    $("#tblCustomer").append(tbody);
+    let customerOb={
+        cusId:id,
+        cusName:name,
+        cusAddress:address,
+        cusSalary:salary
+    }
 
+    customerDB.push(customerOb);
+
+    //let tbody = "<tr>" + "<td>" + id + "</td>" + "<td>" + name + "</td>" + "<td>" + address + "</td>" + "<td>" + salary + "</td>" + "</tr>";
+
+    /*let tbody=`<tr>
+                        <td>${customer[0]}</td>
+                        <td>${customer[1]}</td>
+                        <td>${customer[2]}</td>
+                        <td>${customer[3]}</td>
+                        </tr>`;
+
+    $("#tblCustomer").append(tbody);*/
+
+});
+
+$("#getAllCustomers").click(function () {
+    $("#tblCustomer").empty();
+
+    for (let i = 0; i < customerDB.length; i++) {
+        let id = customerDB[i].cusId;
+        let name = customerDB[i].cusName;
+        let address = customerDB[i].cusAddress;
+        let salary = customerDB[i].cusSalary;
+
+        let row=`<tr>
+                        <td>${id}</td>
+                        <td>${name}</td>
+                        <td>${address}</td>
+                        <td>${salary}</td>
+                        </tr>`;
+
+        $("#tblCustomer").append(row);
+    }
 });
 
 /*document.getElementById("saveCustomer").addEventListener("click", function () {
--- assignments/js/inMemory-POS/assets/js/ItemScript.js
+++ assignments/js/inMemory-POS/assets/js/ItemScript.js
@@ -1,13 +1,46 @@
+var itemDB=[];
+
 $("#saveItem").click(function () {
     let code = $("#item-ID").val();
     let productName = $("#item-Name").val();
     let price = $("#item-Price").val();
     let qty = $("#item-Quantity").val();
 
-    let tbody = "<tr>" + "<td>" + code + "</td>" + "<td>" + productName + "</td>" + "<td>" + price + "</td>" + "<td>" + qty + "</td>" + "</tr>";
+    // let item=[code,productName,price,qty];
 
-    $("#tblItem").append(tbody);
+    let ItemOb={
+        iCode:code,
+        iName:productName,
+        iPrice:price,
+        iQty:qty
+    }
 
+    itemDB.push(ItemOb);
+
+    /*let tbody = "<tr>" + "<td>" + code + "</td>" + "<td>" + productName + "</td>" + "<td>" + price + "</td>" + "<td>" + qty + "</td>" + "</tr>";
+
+    $("#tblItem").append(tbody);*/
+
+});
+
+$("#getAllItems").click(function () {
+    $("#tblItem").empty();
+
+    for (let i = 0; i < itemDB.length; i++) {
+        let code = itemDB[i].iCode;
+        let productName = itemDB[i].iName;
+        let unitPrice = itemDB[i].iPrice;
+        let qty = itemDB[i].iQty;
+
+        let row=`<tr>
+                        <td>${code}</td>
+                        <td>${productName}</td>
+                        <td>${unitPrice}</td>
+                        <td>${qty}</td>
+                        </tr>`;
+
+        $("#tblItem").append(row);
+    }
 });
 /*document.getElementById("saveItem").addEventListener("click", function () {
 
