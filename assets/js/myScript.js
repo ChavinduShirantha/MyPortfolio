ff3853241cf12c5e7e17a0b901a3aef6bf96cf2a
ChavinduShirantha <chavindushirantha@gmail.com>
2023-06-17 19:58:08 +0530

    Updated Portfolio Certificate Section

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -31,3 +31,55 @@ function animateText() {
     }
 
 };
+
+const slidesContainer = document.querySelector('.slides-container');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth;
+let currentSlide = 0;
+let intervalId;
+
+// Show the initial slide
+slides[currentSlide].classList.add('active');
+
+function goToSlide(slideIndex) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = slideIndex;
+    slides[currentSlide].classList.add('active');
+
+    const transformValue = -slideWidth * currentSlide;
+    slidesContainer.style.transform = `translateX(${transformValue}px)`;
+}
+
+function previousSlide() {
+    const newIndex = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
+    goToSlide(newIndex);
+}
+
+function nextSlide() {
+    const newIndex = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
+    goToSlide(newIndex);
+}
+
+function startSlideshow() {
+    intervalId = setInterval(nextSlide, 5000);
+}
+
+function stopSlideshow() {
+    clearInterval(intervalId);
+}
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    stopSlideshow();
+    previousSlide();
+});
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    stopSlideshow();
+    nextSlide();
+});
+
+startSlideshow();
+
+document.getElementById('slideshow').addEventListener('mouseover', stopSlideshow);
+
+document.getElementById('slideshow').addEventListener('mouseout', startSlideshow);
